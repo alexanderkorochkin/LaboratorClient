e17c1c1790c0e75c708c8a8a8f2c93372f9da9c6
Alexander Korochkin <coro4kin2010@gmail.com>
2021-11-01 14:44:58 +0300

    v0.2

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
 import socket
 
-# from opcua import Client
+from opcua import Client
 from kivy.app import App
+from kivy.clock import Clock
 # from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
+# from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+# from kivy.uix.label import Label
+from kivy.properties import StringProperty
+from kivy.lang import Builder
+
+Builder.load_file('main.kv')
 
 
-def get_local_ip():
-    return [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in
-            [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+class ClientUPCUA(Client):
+
+    def __init__(self, url, **kwargs):
+        super().__init__(url)
+        self.connect()
+        self.root = self.get_root_node()
+        self.myvar = self.root.get_child(["0:Objects", "2:MyObject", "2:MyVariable"])
+        self.obj = self.root.get_child(["0:Objects", "2:MyObject"])
+
+
+class BoxIncoming(Widget):
+    text_incoming = StringProperty()
+
+    def __init__(self, **kwargs):
+        super(BoxIncoming, self).__init__(**kwargs)
+        self.text_incoming = "none"
+
+    def changeText(self, text):
+        self.text_incoming = text
+
+
+def update_it(self):
+    text = str(LaboratorClientApp().Clienter.myvar.get_value())
+    LaboratorClientApp().Box.changeText(text)
 
 
 class LaboratorClientApp(App):
+    Clienter = ClientUPCUA("opc.tcp://" + "192.168.1.67" + ":4840/freeopcua/server/")
+    Box = BoxIncoming()
 
     def build(self):
-        layoutStart = BoxLayout(orientation='vertical')
-        ip_label = Label(text=get_local_ip(), font_size=12)
-        text_label = Label(text=get_local_ip(), font_size=12)
-        layoutStart.add_widget(ip_label)
-        layoutStart.add_widget(text_label)
-        return layoutStart
+        return self.Box
 
     def on_start(self):
-        pass
+        Clock.schedule_interval(update_it, 1)
 
     def on_stop(self):
         pass
