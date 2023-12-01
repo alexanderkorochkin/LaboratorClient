7ab2cc17fb461aacdfb4def69980e82bcca8b4bc
XooXricky <coro4kin2010@gmail.com>
2023-12-01 13:51:59 +0600

    v4.12 - dev closed

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from libs.kivyapp import KivyApp
+import os, sys
+from kivy.resources import resource_add_path, resource_find
+
 
 if __name__ == "__main__":
+    if hasattr(sys, '_MEIPASS'):
+        resource_add_path(os.path.join(sys._MEIPASS))
     KivyApp.run()
