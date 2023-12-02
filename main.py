04cd3676f09bc2e99a1c9453db8bfbe11636f41e
XooXricky <coro4kin2010@gmail.com>
2023-12-02 10:35:27 +0600

    v4.12 - dev closed

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import sys
 
 from kivy.resources import resource_add_path
+from kivy.utils import platform
 
 from libs.kivyapp import KivyApp
 
 if __name__ == "__main__":
-    if hasattr(sys, '_MEIPASS'):
+    if hasattr(sys, '_MEIPASS') and platform != 'android':
         resource_add_path(os.path.join(sys._MEIPASS))
     KivyApp.run()
